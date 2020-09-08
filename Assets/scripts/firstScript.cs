f09d1771322d1833fd54308556d450fe4fc3d980
FSD38\levi.may <levimay567@gmail.com>
2020-09-08 11:01:04 -0600

    Initial commit

---
+++ Assets/scripts/firstScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firstScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hello World");
+        //hay
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
