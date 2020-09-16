bc4092f956769d75f42974331a8e66e58ed511be
FSD38\levi.may <levimay567@gmail.com>
2020-09-16 09:33:51 -0600

    Seccond Commit

---
+++ Assets/scripts/MenuMan.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMan : MonoBehaviour {
+    //Create Public verrables (Game object)
+   public GameObject player;
+   public GameObject mainMenu;
+
+    //private Boolean onStartGamerSelection;
+
+
+	// Use this for initialization
+	void Start () {
+        player.SetActive(false);
+        mainMenu.SetActive(true);
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+
+        if (Input.GetKey("space") || Input.GetKey("enter"))
+        {
+            player.SetActive(true);
+            mainMenu.SetActive(false);
+        }
+        // if player presses up move the seletion up
+
+        //if the player presses down move the selection down
+
+        // if the player presses space and over start start the game
+
+        //if the player presses space on player selecter change the number
+
+    }
+}
---
+++ Assets/scripts/PlayerManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    public float speed;
+    SpriteRenderer SpriteRen;
+    Animator Tankanim;
+
+    //devlare verriables
+    //
+    
+	// Use this for initialization
+	void Start () {
+        Debug.Log("hello");
+
+        rb2d = GetComponent<Rigidbody2D>();
+        SpriteRen = GetComponent<SpriteRenderer>();
+        Tankanim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //get user input
+
+        //if user pushes up move person up
+        //check if up key is pressed
+        //change velosity
+        //play animateion
+
+        //if user pushes down key move person up
+        //check if up key is pressed
+        //change velosity
+        //play animateion
+        //flip sprite y
+
+        //if user pushes Right move person Right
+        //check if up key is pressed
+        //change velosity
+        //play animateion
+
+        //if userpushes up left person left
+        //check if up key is pressed
+        //change velosity
+        //play animateion
+        //flip sprite x
+
+
+
+        ;
+        if (Input.GetKey("w") || Input.GetKey("up") )
+        {
+            rb2d.velocity = new Vector2(0, speed);
+            SpriteRen.flipY = false;
+            Tankanim.Play("TankMoveUp");
+        }
+       else if (Input.GetKey("s") || Input.GetKey("down"))
+        {
+            rb2d.velocity = new Vector2(0, -speed);
+            SpriteRen.flipY = true;
+            Tankanim.Play("TankMoveUp");
+        }
+        else if (Input.GetKey("d")  || Input.GetKey("right") )
+        {
+            rb2d.velocity = new Vector2(speed, 0);
+            SpriteRen.flipX = false;
+            Tankanim.Play("TankMoveRight");
+        }
+        else if (Input.GetKey("a") || Input.GetKey("left") )
+        {
+            rb2d.velocity = new Vector2(-speed, 0);
+            SpriteRen.flipX = true;
+            Tankanim.Play("Tank" +
+                "MoveRight");
+        }
+        else
+        {
+            rb2d.velocity = new Vector2(0, 0);
+        }
+
+
+
+    }
+}
